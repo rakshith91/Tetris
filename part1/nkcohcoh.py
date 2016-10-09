fb3b6369362c9b4c5d6baf7a699f0da1f0738134
=Sairam Rakshith Bhyravabhotla <=bsairamr@iu.edu>
2016-10-09 05:07:34 +0000

    basic skeleton

---
+++ part1/nkcohcoh.py
@@ -0,0 +1,19 @@
+import sys
+n = sys.argv[1]
+k = sys.argv[2]
+node = sys.argv[3]
+time_limit = sys.argv[4]
+
+def printable_board(node):
+
+def is_goal(board):
+
+def add_black_marble(board, row, col):
+
+def add_white_marble(board, row, col):
+
+def successors(state):
+
+def heuristic(state):
+
+def solve(state):
