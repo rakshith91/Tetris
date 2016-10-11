cee5f1a85cf45be43d211d1b9f92919b984eea0d
=Sairam Rakshith Bhyravabhotla <=bsairamr@iu.edu>
2016-10-11 13:54:14 +0000

    comments and to do

--- part1/nkcohcoh.py
+++ part1/nkcohcoh.py
@@ -1,32 +1,48 @@
+''' TO DO
+1)write a function to check symmetry. we should not generate successors which are symmetrical to 
+existing successors
+
+2)write a function to check no of continuous w's , b's and .'s in a row, col and diagonal(assigned to Rakshith)
+
+3) develop ( and code) a heuristic. 
+
+4)write minimax / a*
+
+PS: keep updating this.
+
+'''
+
+
+
 import sys
 n = int(sys.argv[1])
 k = int(sys.argv[2])
 node = sys.argv[3]
 time_limit = sys.argv[4]
+#determine which color takes the move. argument node is a string input from sys(refer line 4)
 def determine_color(node):
 	whites = node.count('w')
 	blacks = node.count('b')
 	return 'w' if whites==blacks else 'b' if whites>blacks else "error"
-
+#converts the string input to a board (list of list) given n(line 2)
 def string_to_board(node, n):
 	return [list(node[i:i+n]) for i in range(0, len(node), n)]		
-
+#function incomplete
 def count_on_row(board, row, color):
 	return sum(board[row])
-
+#function incomplete
 def count_on_col(board,col):
 	return sum([row[col] for row in board])
 
 #def count_on_diagonals(board, row, col):
+
 #idea for this function taken from assignment 0
 def add_marble(board, row, col, color):
 	return  board[0:row] + [board[row][0:col] + [color,] + board[row][col+1:]] + board[row+1:]
-
+#successors for a given input. we should avoid symmetrical boards. so , we should write a function
+#to check symmetry
 def successors(board):	
 	return [add_marble(board, row, col, color) for row in range(0,n) for col in range(0,n) if board[row][col]=='.' ]
-color= determine_color(node)
-board= string_to_board(node,n)
-print successors(board)
 
 #def is_goal(board):
 
