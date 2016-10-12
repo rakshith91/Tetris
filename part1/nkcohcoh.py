6798082b37c0c350e261c1cd97e683952a167ffa
Prateek Srivastava <pratsriv@iu.edu>
2016-10-12 14:55:24 -0400

    merged

--- part1/nkcohcoh.py
+++ part1/nkcohcoh.py
@@ -1,4 +1,4 @@
-''' TO DO
+'''
 1)write a function to check symmetry. we should not generate successors which are symmetrical to 
 existing successors
 
@@ -14,7 +14,7 @@ PS: keep updating this.
 from copy import deepcopy
 import sys
 from itertools import groupby
-from itertools import groupby
+
 n = int(sys.argv[1])
 k = int(sys.argv[2])
 node = sys.argv[3]
@@ -30,6 +30,13 @@ def determine_color(node):
 def string_to_board(node, n):
 	return [list(node[i:i+n]) for i in range(0, len(node), n)]
 
+#function incomplete
+def count_on_row(board, row, color):
+	return [[(i,len(list(g))) for i,g in groupby(row)] for row in board]
+
+#function incomplete
+def count_on_col(board,col):
+	return [[(i,len(list(g))) for i,g in groupby(col)] for col in zip(*board)]
 
 #def count_on_diagonals(board, row, col):
 
@@ -83,68 +90,18 @@ def count_on_diag(board, row, col,n,k,color):
 
 # return sum(board[row])
 
-# evaluation function
-def eval(board,color,n,k):
-	print board,color
-	row_count=[]
-	col_count=[]
-	diag_count=[]
-	for row in range(0,n):
-		row_count.append(count_on_row(board,row,color))
-		for col in range(0,n):
-			count_on_diag(board,row,col,n,k,color)
-	# col_count+=count_on_col(board,col,color)
-	for col in range(0,n):
-		col_count.append(count_on_col(board, col, color,col_count))
-
-	# print row_count,col_count
-
-# Count # of pieces in given column
-def count_on_col(board, col,color,count_col):
-	# print [board[i][col] for i in range(0,n)].count(color)
-	count_col=[board[i][col] for i in range(0,n)].count(color)
-	return count_col
-
-# Count # of pieces in given row
-def count_on_row(board, row, color):
-	return board[row].count(color)
-
-def count_on_diag(board, row, col,n,k,color):
-	# print board
-	for r in range(0,n):
-		for c in range(0,n):
-			left_upper_diagonal = 0
-			left_bottom_diagonal = 0
-			right_upper_diagonal = 0
-			right_bottom_diagonal = 0
-			for i in range(1,k):
-				if(row-i>=0 and col-i>=0):
-
-					left_upper_diagonal+=(board[row-i][col-i].count(color))
-					print left_upper_diagonal,r,c
-				# if(row+i<k and col+i<k):
-				# 	right_bottom_diagonal=(board[row+i][col+i].count(color))
-				# if (row - i >= 0 and col + i <k ):
-				# 	right_upper_diagonal=(board[row-i][col+i].count(color))
-				# if (row + i <k and col - i >= 0):
-				# 	left_bottom_diagonal=(board[row+i][col-i].count(color))
-				# i += 1
-	# print left_upper_diagonal,right_bottom_diagonal,right_upper_diagonal,left_bottom_diagonal
-
-
-# return sum(board[row])
-
 # checks the symmetry
-# def check_symmetry(a,n):
-# 	b = deepcopy(a)
-# 	for col in range(0,n):
-# 		for row in range(n-1,-1,-1):
-# 			for row1 in range(0,n):
-# 				for col1 in range(0,n):
-# 					b[row1][col1]=a[col][row]
-# 	print b
+def check_symmetry(a,n):
+	b = deepcopy(a)
+	for col in range(0,n):
+		for row in range(n-1,-1,-1):
+			for row1 in range(0,n):
+				for col1 in range(0,n):
+					b[row1][col1]=a[col][row]
+	print b
 
 #def solve(state):
 color = determine_color(node)
-successor=successors(string_to_board(node,n))
-eval(successor[0],color,n,k)
+print successors(string_to_board(node,n))
+
+
