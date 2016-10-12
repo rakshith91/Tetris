167c708488b93e0ddc182e247edf3167216c66c2
Prateek Srivastava <pratsriv@iu.edu>
2016-10-11 20:36:43 -0400

    Merge branch 'master' of /Users/prateeksrivastava/Documents/git/CSCI_551/a2 with conflicts.

--- part1/nkcohcoh.py
+++ part1/nkcohcoh.py
@@ -13,7 +13,7 @@ PS: keep updating this.
 '''
 
 
-from itertools import groupby
+
 import sys
 n = int(sys.argv[1])
 k = int(sys.argv[2])
@@ -30,25 +30,20 @@ def determine_color(node):
 def string_to_board(node, n):
 	return [list(node[i:i+n]) for i in range(0, len(node), n)]		
 
-#counts continous values of whites and blacs and dots in all row
-def count_on_row(board):
-	return [[(i,len(list(g))) for i,g in groupby(row)] for row in board]
+#function incomplete
+def count_on_row(board, row, color):
+	return sum(board[row])
+#function incomplete
+def count_on_col(board,col):
+	return sum([row[col] for row in board])
 
-#counts continous values of whites and blacs and dots in all cols
-def count_on_col(board):
-	 return [[(i,len(list(g))) for i,g in groupby(col)] for col in zip(*board)]
-
-#counts continous values of whites , blacks and dots in all diagonals
-#def count_on_diagonals(board):
+#def count_on_diagonals(board, row, col):
 
 #idea for this function taken from assignment 0
 def add_marble(board, row, col, color):
 	return  board[0:row] + [board[row][0:col] + [color,] + board[row][col+1:]] + board[row+1:]
-
 #successors for a given input. we should avoid symmetrical boards. so , we should write a function
 #to check symmetry
-
-
 def successors(board):	
 	return [add_marble(board, row, col, color) for row in range(0,n) for col in range(0,n) if board[row][col]=='.' ]
 
@@ -56,4 +51,10 @@ def successors(board):
 
 #def heuristic(state):
 
+def check_symmetry(board_1,board_2):
+	print "hello"
+
 #def solve(state):
+color = determine_color(node)
+check_symmetry([1,0],[0,])
+#print successors(string_to_board(node,n))
