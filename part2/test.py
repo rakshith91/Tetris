68b862c11f5ba10102db8f3e6a27d3402bab0390
=Sairam Rakshith Bhyravabhotla <=bsairamr@iu.edu>
2016-10-18 06:53:38 +0000

    working soln for simple version, must design a good heuristic

---
+++ part2/test.py
@@ -0,0 +1,4 @@
+
+board = ['          ', '          ', '      xx x', '      xxxx', ' xxxxxxxx ', '   xxxxxxx', ' xxxx xxxx', ' xxxx xxxx', 'xxxx xx xx', 'x  xxxxxxx', 'xx xxxx x ', 'xxxxxxxxx ', 'xxxxxx xxx', 'xxxxxxxxx ', 'xx xxx xxx', 'xx  xxxxx ', 'xxx   x xx', ' xxxxxxxxx', 'xx  xxx x ', 'xxx  xxxxx']
+				
+print hole(board)
